6f704b4f7ce5a91aa9e7c83dd3765584606dd33e
Zafar <zafar142007@gmail.com>
2015-03-12 17:22:25 +0530

    indenting

--- TwitterVisualizer/src/main/java/com/zafar/TwitterReader.java
+++ TwitterVisualizer/src/main/java/com/zafar/TwitterReader.java
@@ -1,4 +1,5 @@
 package com.zafar;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,53 +15,63 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Controller
 public class TwitterReader {
 
-	private static Logger log=Logger.getLogger(TwitterReader.class);
+	private static Logger log = Logger.getLogger(TwitterReader.class);
 	private HashMap<Position, Client> clients;
-	private OAuthHeaders headers=new OAuthHeaders();
-	public TwitterReader(){
-		clients=new HashMap<Position, Client>();
+	private OAuthHeaders headers = new OAuthHeaders();
+
+	public TwitterReader() {
+		clients = new HashMap<Position, Client>();
 	}
-	@RequestMapping(value="/start",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void startStreamingFromTwitter(@RequestParam(value = "lat", required = true) String lat, @RequestParam(value = "longi", required = true) String longi){
-		Client client=new Client(lat, longi);
-		Position pos=new Position(lat, longi);
-		if(clients.get(pos)==null)
-		{	
+	public void startStreamingFromTwitter(
+			@RequestParam(value = "lat", required = true) String lat,
+			@RequestParam(value = "longi", required = true) String longi) {
+		Client client = new Client(lat, longi);
+		Position pos = new Position(lat, longi);
+		if (clients.get(pos) == null) {
 			clients.put(pos, client);
-			TwitterThreadProvider provider=client.getProvider();
+			TwitterThreadProvider provider = client.getProvider();
 			provider.setHeaders(headers);
 			provider.start();
 		}
 	}
-	@RequestMapping(value="/end",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/end", method = RequestMethod.GET)
 	@ResponseStatus(value = HttpStatus.OK)
-	public void stopStreamingFromTwitter(@RequestParam(value = "lat", required = true) String lat, @RequestParam(value = "longi", required = true) String longi){
-		Position pos=new Position(lat, longi);
-		Client c=clients.remove(pos);
+	public void stopStreamingFromTwitter(
+			@RequestParam(value = "lat", required = true) String lat,
+			@RequestParam(value = "longi", required = true) String longi) {
+		Position pos = new Position(lat, longi);
+		Client c = clients.remove(pos);
 		c.getProvider().stopMe();
-		log.info("Ended feed filtering for "+lat+" and "+longi);
+		log.info("Ended feed filtering for " + lat + " and " + longi);
 	}
-	@RequestMapping(value="/tweet",method=RequestMethod.GET)
-	public @ResponseBody Tweet[] getTweet(@RequestParam(value="index", required=true) String indexx, @RequestParam(value = "lat", required = true) String lat, @RequestParam(value = "longi", required = true) String longi){
-		Client c=clients.get(new Position(lat, longi));
-		if(c!=null)
-		{
-			ArrayList<String> tweets=c.getTweets();
-			Tweet newTweets[]=null;
-			int index=Integer.parseInt(indexx);
-			if(tweets!=null)
-			{	synchronized(tweets){
-					if(tweets.size()-index >0)
-					{	newTweets=new Tweet[tweets.size()-index];
-					
-						for(int i=index, j=0; i<tweets.size(); i++, j++)
-							newTweets[j]=new Tweet(tweets.get(i));
+
+	@RequestMapping(value = "/tweet", method = RequestMethod.GET)
+	public @ResponseBody
+	Tweet[] getTweet(
+			@RequestParam(value = "index", required = true) String indexx,
+			@RequestParam(value = "lat", required = true) String lat,
+			@RequestParam(value = "longi", required = true) String longi) {
+		Client c = clients.get(new Position(lat, longi));
+		if (c != null) {
+			ArrayList<String> tweets = c.getTweets();
+			Tweet newTweets[] = null;
+			int index = Integer.parseInt(indexx);
+			if (tweets != null) {
+				synchronized (tweets) {
+					if (tweets.size() - index > 0) {
+						newTweets = new Tweet[tweets.size() - index];
+
+						for (int i = index, j = 0; i < tweets.size(); i++, j++)
+							newTweets[j] = new Tweet(tweets.get(i));
 					}
 				}
 			}
 			return newTweets;
-		}
-		else return null;
+		} else
+			return null;
 	}
 }
--- TwitterVisualizer/src/main/java/com/zafar/TwitterThreadProvider.java
+++ TwitterVisualizer/src/main/java/com/zafar/TwitterThreadProvider.java
@@ -14,135 +14,158 @@ import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 
 public class TwitterThreadProvider extends Thread {
-	private Logger log=Logger.getLogger(TwitterThreadProvider.class);
+	private Logger log = Logger.getLogger(TwitterThreadProvider.class);
 	volatile boolean finished = false;
-	private String consumerKey="", consumerSecret="", accessTokenString="", accessTokenSecret="";
+	private String consumerKey = "", consumerSecret = "",
+			accessTokenString = "", accessTokenSecret = "";
 	private String longitude, latitude;
-	private double RADIUS=1.0;//search within 1 mile
-	private final double EARTH_RADIUS=3959.0;//earth radius in miles
-	private int MAX_NUMBER_OF_TWEETS=100;
-    private static final String STREAM_URI = "https://stream.twitter.com/1.1/statuses/filter.json";
-    public ArrayList<String> tweets;
-    public void setHeaders(OAuthHeaders h)
-    {
-    	setConsumerKey(h.getConsumerKey());
-    	setConsumerSecret(h.getConsumerSecret());
-    	setAccessTokenSecret(h.getAccessTokenSecret());
-    	setAccessTokenString(h.getAccessTokenString());
-    }
+	private double RADIUS = 1.0;// search within 1 mile
+	private final double EARTH_RADIUS = 3959.0;// earth radius in miles
+	private int MAX_NUMBER_OF_TWEETS = 100;
+	private static final String STREAM_URI = "https://stream.twitter.com/1.1/statuses/filter.json";
+	public ArrayList<String> tweets;
+
+	public void setHeaders(OAuthHeaders h) {
+		setConsumerKey(h.getConsumerKey());
+		setConsumerSecret(h.getConsumerSecret());
+		setAccessTokenSecret(h.getAccessTokenSecret());
+		setAccessTokenString(h.getAccessTokenString());
+	}
+
 	private void setConsumerKey(String consumerKey) {
 		this.consumerKey = consumerKey;
 	}
+
 	private void setConsumerSecret(String consumerSecret) {
 		this.consumerSecret = consumerSecret;
 	}
+
 	private void setAccessTokenString(String accessTokenString) {
 		this.accessTokenString = accessTokenString;
 	}
+
 	private void setAccessTokenSecret(String accessTokenSecret) {
 		this.accessTokenSecret = accessTokenSecret;
 	}
-    public void setTweets(ArrayList<String> t){
-    	 tweets=t;
-    }
-    public void setRadius(double d){
-    	RADIUS=d;
-    }
-    public TwitterThreadProvider() {
-	}
-    
-    public TwitterThreadProvider(String lat, String longi, ArrayList<String> tweets){
-    	latitude=lat;
-    	longitude=longi;
-    	this.tweets=tweets;
-    }
-	public void setLatitude(String lat) {
-		latitude=lat;
+
+	public void setTweets(ArrayList<String> t) {
+		tweets = t;
 	}
 
-	public void setLongitude(String longi){
-		longitude=longi;
+	public void setRadius(double d) {
+		RADIUS = d;
 	}
-	public static double convertToRadians(double degree){
-		return degree*Math.PI/180.0;
+
+	public TwitterThreadProvider() {
 	}
-	public static double convertToDegrees(double radians){
-		return radians*180.0/Math.PI;
+
+	public TwitterThreadProvider(String lat, String longi,
+			ArrayList<String> tweets) {
+		latitude = lat;
+		longitude = longi;
+		this.tweets = tweets;
 	}
-	public String calculateBoundingBox(){
-		StringBuilder result=new StringBuilder();
-		try{
-			double lat=Double.parseDouble(latitude);
-			double longi=Double.parseDouble(longitude);
-	
-			result.append(convertToDegrees(convertToRadians(longi)-(RADIUS/EARTH_RADIUS)));
+
+	public void setLatitude(String lat) {
+		latitude = lat;
+	}
+
+	public void setLongitude(String longi) {
+		longitude = longi;
+	}
+
+	public static double convertToRadians(double degree) {
+		return degree * Math.PI / 180.0;
+	}
+
+	public static double convertToDegrees(double radians) {
+		return radians * 180.0 / Math.PI;
+	}
+
+	public String calculateBoundingBox() {
+		StringBuilder result = new StringBuilder();
+		try {
+			double lat = Double.parseDouble(latitude);
+			double longi = Double.parseDouble(longitude);
+
+			result.append(convertToDegrees(convertToRadians(longi)
+					- (RADIUS / EARTH_RADIUS)));
 			result.append(",");
-			result.append(convertToDegrees(convertToRadians(lat)-(RADIUS/EARTH_RADIUS)));
-			
+			result.append(convertToDegrees(convertToRadians(lat)
+					- (RADIUS / EARTH_RADIUS)));
+
 			result.append(",");
-			
-			result.append(convertToDegrees(convertToRadians(longi)+(RADIUS/EARTH_RADIUS)));
+
+			result.append(convertToDegrees(convertToRadians(longi)
+					+ (RADIUS / EARTH_RADIUS)));
 			result.append(",");
-			result.append(convertToDegrees(convertToRadians(lat)+(RADIUS/EARTH_RADIUS)));
-		}
-		catch(Exception e){
+			result.append(convertToDegrees(convertToRadians(lat)
+					+ (RADIUS / EARTH_RADIUS)));
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result.toString();
 	}
-	public void run(){
-		try{
+
+	public void run() {
+		try {
 			OAuthService service = new ServiceBuilder()
-	        .provider(TwitterApi.class)
-	        .apiKey(consumerKey)
-	        .apiSecret(consumerSecret)
-	        .build();
-			String searchBoundingBox=calculateBoundingBox();
+					.provider(TwitterApi.class).apiKey(consumerKey)
+					.apiSecret(consumerSecret).build();
+			String searchBoundingBox = calculateBoundingBox();
 			Token accessToken = new Token(accessTokenString, accessTokenSecret);
 
-            // Let's generate the request
-            log.debug("Connecting");
-            OAuthRequest request = new OAuthRequest(Verb.POST, STREAM_URI);
-            request.addHeader("version", "HTTP/1.1");
-            request.addHeader("host", "stream.twitter.com");
-            request.setConnectionKeepAlive(true);
-            request.addHeader("user-agent", "Twitter Stream Reader");
-            request.addBodyParameter("locations",searchBoundingBox); // Set keywords you'd like to track here
-            service.signRequest(accessToken, request);
-            log.debug(request);
-            Response response = request.send();
+			// Let's generate the request
+			log.debug("Connecting");
+			OAuthRequest request = new OAuthRequest(Verb.POST, STREAM_URI);
+			request.addHeader("version", "HTTP/1.1");
+			request.addHeader("host", "stream.twitter.com");
+			request.setConnectionKeepAlive(true);
+			request.addHeader("user-agent", "Twitter Stream Reader");
+			request.addBodyParameter("locations", searchBoundingBox); // Set
+																		// keywords
+																		// you'd
+																		// like
+																		// to
+																		// track
+																		// here
+			service.signRequest(accessToken, request);
+			log.debug(request);
+			Response response = request.send();
 
-            // Create a reader to read Twitter's stream
-            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getStream()));
+			// Create a reader to read Twitter's stream
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					response.getStream()));
 
-            String line;
-            while ((line = reader.readLine()) != null && finished==false) {
-            	synchronized(tweets){
-            		if(line!="")
-            		{	
-            			tweets.add(line);
-            			if(tweets.size()==MAX_NUMBER_OF_TWEETS)
-            			{
-            				tweets.remove(0);//flush out from the front of the queue
-            			}
-            		}
-            	}
-            	log.info(line);
-            }
-            reader.close();
-            log.debug("Closing this thread: latitude "+latitude+"  longitude "+longitude);
-		}
-		catch(Exception e){
+			String line;
+			while ((line = reader.readLine()) != null && finished == false) {
+				synchronized (tweets) {
+					if (line != "") {
+						tweets.add(line);
+						if (tweets.size() == MAX_NUMBER_OF_TWEETS) {
+							tweets.remove(0);// flush out from the front of the
+												// queue
+						}
+					}
+				}
+				log.info(line);
+			}
+			reader.close();
+			log.debug("Closing this thread: latitude " + latitude
+					+ "  longitude " + longitude);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public void stopMe()
-	{
-	    finished = true;
+
+	public void stopMe() {
+		finished = true;
 	}
-	public boolean isFinished(){
+
+	public boolean isFinished() {
 		return isAlive();
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -153,6 +176,7 @@ public class TwitterThreadProvider extends Thread {
 				+ ((longitude == null) ? 0 : longitude.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
